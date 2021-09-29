b14e0ace012dcb7bfab906a3d5d8a1e86db4be8d
lucho19jose <barbozagonzalesjose@gmail.com>
2021-09-29 11:34:17 -0500

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,16 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js']
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?.js/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
 }
