334227fa3de49e8db133047ac27ca729389b7e1f
lucho19jose <barbozagonzalesjose@gmail.com>
2021-09-29 12:02:31 -0500

    css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 /** @type {import('webpack').Configuration} */
 module.exports = {
@@ -19,6 +20,11 @@ module.exports = {
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader'],
       }
     ]
   },
@@ -27,6 +33,7 @@ module.exports = {
       inject: true,
       template: './public/index.html',
       filename: './index.html'
-    })
+    }),
+    new MiniCssExtractPlugin(),
   ]
 }
