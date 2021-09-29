ea4f2acce8932c42269e4c6e74181db97e97db76
lucho19jose <barbozagonzalesjose@gmail.com>
2021-09-29 07:54:14 -0500

    img loaders & fonts loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve: {
     extensions: ['.js']
@@ -30,6 +31,24 @@ module.exports = {
           'stylus-loader'
         ]
       },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            mimetype: "application/font-woff",
+            name: "[name].[ext]",
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts/",
+            esModule: false,
+          },
+        }
+      }
     ]
   },
   plugins: [
