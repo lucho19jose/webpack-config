ac3427054f4d012530ba18f3635ac29bbddd7885
lucho19jose <barbozagonzalesjose@gmail.com>
2021-10-01 09:56:50 -0500

    .env and mode dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,8 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 const Dotenv = require('dotenv-webpack')
 
 /** @type {import('webpack').Configuration} */
@@ -14,6 +12,7 @@ module.exports = {
     filename: '[name][contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
+  mode: 'development',
   resolve: {
     extensions: ['.js'],
     alias: {
@@ -75,12 +74,5 @@ module.exports = {
       ]
     }),
     new Dotenv()
-  ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin(),
-    ]
-  }
+  ]
 }
